cb7f91fd54e0859054a65c1221453cfd2d440664
yemik <oduyemik@outlook.com>
2025-11-07 18:39:40 +0000

    Update miryoku.h

--- keyboards/crkbd/keymaps/miryoku_custom/miryoku.h
+++ keyboards/crkbd/keymaps/miryoku_custom/miryoku.h
@@ -1,4 +1,4 @@
-#include "miryoku_layer_list.h"
-#include "miryoku_layer_selection.h"
-#include "miryoku_layer_alternatives.h"
+#include "miryoku_babel/miryoku_layer_list.h"
+#include "miryoku_babel/miryoku_layer_selection.h"
+#include "miryoku_babel/miryoku_layer_alternatives.h"
 #include "manna-harbour_miryoku.h"
