f46b0e35b9ad8bb05ae554eef0cd7f71a0a8c082
yemik <oduyemik@outlook.com>
2025-11-07 15:57:59 +0000

    Update keymap.c

--- keyboards/crkbd/keymaps/miryoku_custom/keymap.c
+++ keyboards/crkbd/keymaps/miryoku_custom/keymap.c
@@ -1,10 +1,5 @@
-#include QMK_KEYBOARD_H
-#include "custom_config.h"
+#include "miryoku.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = MIRYOKU_LAYER_BASE,
-    [1] = MIRYOKU_LAYER_NUM,
-    [2] = MIRYOKU_LAYER_SYM,
-    [3] = MIRYOKU_LAYER_FUN,
-    [4] = MIRYOKU_LAYER_BUTTON
+    MIRYOKU_QWERTY
 };
